0f05976d7f8c5a8778549bd25ba9145dfb711994
rroesmal <37796303+rroesmal@users.noreply.github.com>
2018-04-16 12:06:49 +0200

    All pages + js forms

---
+++ js/code.js
@@ -0,0 +1,7 @@
+
+function submitNieuwsbriefForm(){
+
+document.getElementById("nieuwsbrief-verificatie").classList.add('hide');
+document.getElementById("aanmeld-bericht").classList.remove('hide');
+
+}
